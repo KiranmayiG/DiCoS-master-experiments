92ed209ea66e1dd7f9d2ad084729b337b59b94a5
KiranmayiG <gkiranmayi97@gmail.com>
2023-02-12 23:40:04 +0530

    Root files

---
+++ train_model.sh
@@ -0,0 +1,20 @@
+python3 train_cpu.py \
+--do_train \
+--output_dir saved_models \
+--save_dir saved_models \
+--load_test_op_data_path cls_score_test_state_update_predictor_output.json \
+--model_name_or_path pretrained_models/albert_base/ \
+--num_workers 2 \
+--n_epochs 30 \
+--data_root data/mwz2.2_100/ \
+--train_data train_dials.json \
+--dev_data test_dials.json \
+--test_data test_dials.json \
+--ontology_data schema.json \
+--per_gpu_train_batch_size 16 \
+--per_gpu_eval_batch_size 16 \
+--enc_lr 2e-5 \
+--base_lr 1e-3 \
+--turn 2 \
+--n_history 1 \
+--eval_step 10000
---
+++ train_model_gpu_turn0_history1.sh
@@ -0,0 +1,20 @@
+torchrun train.py \
+--do_train \
+--output_dir gpu_saved_models_28_01_turn0 \
+--save_dir gpu_saved_models_28_01_turn0 \
+--load_test_op_data_path cls_score_test_state_update_predictor_output.json \
+--model_name_or_path pretrained_models/albert_base/ \
+--n_epochs 5 \
+--local_rank 0 \
+--data_root data/mwz2.2/ \
+--train_data train_dials.json \
+--dev_data dev_dials.json \
+--test_data test_dials.json \
+--ontology_data schema.json \
+--per_gpu_train_batch_size 32 \
+--per_gpu_eval_batch_size 32 \
+--enc_lr 2e-5 \
+--base_lr 1e-3 \
+--turn 0 \
+--n_history 1 \
+--eval_step 10000
