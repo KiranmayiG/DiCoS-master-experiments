a4752a703fa4a7a3b6b38bef192c8428cd9e7e34
KiranmayiG <gkiranmayi97@gmail.com>
2023-02-12 23:39:06 +0530

    Main bash files

---
+++ eval_model.sh
@@ -0,0 +1,13 @@
+torchrun train.py  \
+--load_model \
+--data_root data/mwz2.2/ \
+--train_data train_dials.json \
+--dev_data dev_dials.json \
+--test_data test_dials.json \
+--ontology_data schema.json \
+--save_dir gpu_saved_models_trial_100_albert_base \
+--load_ckpt_epoch checkpoint_epoch_5.bin \
+--load_test_op_data_path cls_score_test_state_update_predictor_output.json \
+--turn 2  \
+--n_history 1 \
+--max_seq_length 256
